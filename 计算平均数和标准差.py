1e3d6e08af224c18b630e3ad89c9a7d50a057cf3
taoliang梁韬 <93470282+ganbingliangyi@users.noreply.github.com>
2022-04-16 09:25:47 +0800

    Update 计算平均数和标准差.py

--- 计算平均数和标准差.py
+++ 计算平均数和标准差.py
@@ -6,31 +6,14 @@
 
 import numpy as np
 import pandas as pd
-filePath_01 = 'C:/Users/46685/Desktop/科研数据/新建处理后后.xlsx'
-a= pd.read_excel(filePath_01,sheet_name = 'Sheet1')
+data = 'C:/Users/46685/Desktop/科研数据/新建处理后后.xlsx'
+a= pd.read_excel(data,sheet_name = 'Sheet1')
 b = a[["年龄","腰围"]]
 print(b)
-
-
-# In[11]:
-
-
 np.mean(b)   #计算平均数
-
-
-# In[12]:
-
-
 np.std(b)   #计算标准差
-
-
-# In[13]:
-
-
 np.std(b)**2    #计算方差
 
-
-# In[ ]:
 
 
 
